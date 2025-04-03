97737c2d51e4a1fcbca379c454421e350626c15d
Alexander-Hirsch <04alexander.hirsch@gmail.com>
2025-04-03 13:13:35 +0200

    ddwdwdw

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -6,6 +6,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react(), tsconfigPaths(), tailwindcss()],
+    base: "/",
+    build:{
+        chunkSizeWarningLimit: 1000,
+    },
     resolve: {
         alias: {
             '@components': '/src/components',
