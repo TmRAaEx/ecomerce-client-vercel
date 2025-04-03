a6570b2fa0439ad91f96474cc6e85c6e62267a7a
Alexander-Hirsch <04alexander.hirsch@gmail.com>
2025-04-03 21:34:20 +0200

    sitemap data

--- my-app/src/components/UI/SearchBar.tsx
+++ my-app/src/components/UI/SearchBar.tsx
@@ -7,6 +7,7 @@ export default function SearchBar() {
 
 
     const [search, setSearch] = useState("");
+    const [results, setResults] = useState([]);
 
 
     const handleSearch = async (e: FormEvent) => {
@@ -17,9 +18,18 @@ export default function SearchBar() {
                     q: search,
                     key: import.meta.env.VITE_GOOGLE_API_KEY,
                     cx: import.meta.env.VITE_SEARCH_ENGINE_ID,
+                    num: 10
                 }
             });
-            console.log(response.data);
+            const data = response.data;
+
+            console.log(data)
+            if (data.items && data.items.length > 0) {
+                setResults(data.items)
+                return
+            }
+            setResults([])
+
         } catch (err) {
             console.log(err);
         }
@@ -27,23 +37,32 @@ export default function SearchBar() {
 
 
     // modified version of https://flowbite.com/docs/forms/search-input/#search-bar-example
-    return <form className="max-w-3xl w-[50%] mx-auto my-auto" onSubmit={handleSearch}>
-        <label htmlFor="default-search"
-               className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-        <div className="relative">
-            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                <SearchIcon/>
-            </div>
-            <input type="search" id="default-search"
-                   className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                   placeholder="Search..."
-                   value={search}
-                   onChange={e => setSearch(e.target.value)}
-            />
+    return <>
+        <form className="max-w-3xl w-[50%] mx-auto my-auto" onSubmit={handleSearch}>
+            <label htmlFor="default-search"
+                   className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+            <div className="relative">
+                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <SearchIcon/>
+                </div>
+                <input type="search" id="default-search"
+                       className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                       placeholder="Search..."
+                       value={search}
+                       onChange={e => setSearch(e.target.value)}
+                />
 
-            <button type="submit"
-                    className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search
-            </button>
-        </div>
-    </form>
+                <button type="submit"
+                        className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search
+                </button>
+            </div>
+        </form>
+
+        {results.length > 0 && (<>
+            {results.map((item) => {
+                item.title
+            })}
+        </>)
+        }
+    </>
 }
